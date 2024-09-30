8cc0729274e87934f8b0f1e6f3bef3de4ed01878
Jules <150710985+Ruuliooo@users.noreply.github.com>
2024-09-30 09:11:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,29 +8,24 @@ const fetchData = async () => {
     const data = await request.json();
     console.log(data.results);
     return data.results;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
-// Insérer les données dans le tableau
 const insertData = async () => {
   const datas = await fetchData();
   for (const data of datas) {
     const row = document.createElement("tr");
     const gender = data.gender;
-    // Ajouter une couleur de fond et une img en fonction du genre
     if (gender === "male") {
-      row.style.backgroundColor = "lightblue";
+      row.style.backgroundColor = "lightyellow";
       genderIcon = "./img/homme.png";
     } else if (gender === "female") {
-      row.style.backgroundColor = "lightpink";
+      row.style.backgroundColor = "lightpurple";
       genderIcon = "./img/femme.png";
     } else {
       row.style.backgroundColor = "lightgreen";
       genderIcon = "./img/other.png";
     }
-    // Ajouts des données dans le tableau
+    
     row.innerHTML = `
       <td>${data.login.username}</td>
       <td><img src = '${genderIcon}'/></td>

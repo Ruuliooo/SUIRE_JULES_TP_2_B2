51a75f465efe935953d681920c2a6e47b247d6e0
Jules <150710985+Ruuliooo@users.noreply.github.com>
2024-10-02 01:02:13 +0200

    Update

--- script.js
+++ script.js
@@ -9,23 +9,28 @@ const fetchData = async () => {
     console.log(data.results);
     return data.results;
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
 };
 
+// Insérer les données dans le tableau
 const insertData = async () => {
   const datas = await fetchData();
   for (const data of datas) {
     const row = document.createElement("tr");
     const gender = data.gender;
+    // Ajouter une couleur de fond et une img en fonction du genre
     if (gender === "male") {
-      row.style.backgroundColor = "pink";
-      genderIcon = "./img/inutile.png";
+      row.style.backgroundColor = "lightblue";
+      genderIcon = "./img/homme.png";
     } else if (gender === "female") {
-      row.style.backgroundColor = "ligtblue";
-      genderIcon = "./img/cuisine.png";
+      row.style.backgroundColor = "lightpink";
+      genderIcon = "./img/femme.png";
+    } else {
+      row.style.backgroundColor = "lightgreen";
+      genderIcon = "./img/other.png";
     }
-
+    // Ajouter les données dans le tableau
     row.innerHTML = `
       <td>${data.login.username}</td>
       <td><img src = '${genderIcon}'/></td>
@@ -34,9 +39,8 @@ const insertData = async () => {
       <td>${data.location.city}</td>
       <td class = 'colonne-country'><img src = 'https://flagsapi.com/${data.nat}/flat/64.png' /> ${data.location.country}</td>
     `;
-  tbody.appendChild(row);
+    tbody.appendChild(row);
   }
 };
 
-
-
+insertData();
